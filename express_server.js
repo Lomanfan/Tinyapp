4e1f85f08feca8049f9bd46ae1a11f1d11d85000
Liuming <lfan.canada@gmail.com>
2021-03-16 21:43:19 +0000

    commit changes

--- express_server.js
+++ express_server.js
@@ -3,8 +3,8 @@
 
 const express = require("express");
 const app = express();
-const PORT = 8080; //default port 8080
-app.set("view engine","ejs");  //Set ejs as the view engine.
+const PORT = 8080;
+app.set("view engine","ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -12,18 +12,36 @@ const urlDatabase = {
 };
 
 
-app.get("/", (req, res) => {   //Notice the line of code that registers a handler on the root path, "/".??
+
+app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {          // add additional endpoints - 
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-//Add the following route:
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls",(req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index",templateVars);
 });
+
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
+
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");  
+// });
+
+app.get("/hello/", (req,res) => {
+  const templateVars = { greeting: 'Hello World!'};
+  res.render("hello_world", templateVars);
+});  
+
 
 
 
