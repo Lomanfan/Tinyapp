6596ff2a35d685f1229556b313a3d148180d69a1
Liuming <lfan.canada@gmail.com>
2021-03-19 00:21:02 +0000

    commit changes

--- express_server.js
+++ express_server.js
@@ -2,15 +2,16 @@
 //Use local host: http://localhost:8080/
 
 const express = require("express");
-const app = express();
-const PORT = 8080;
-app.set("view engine","ejs");
-const morgan = require('morgan');
-app.use(morgan("dev"));
-const { restart } = require('nodemon');
-
-
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
+const app = express();
+const PORT = process.env.PORT || 8080;
+const { restart } = require('nodemon');
+const { response } = require("express");
+app.set("view engine","ejs");
+app.use(morgan("dev"));
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
@@ -20,11 +21,36 @@ const urlDatabase = {
 };    
 
 
+const users = {
+  '2cc689cc': {     //random id 8 digits
+    id: '2cc689cc',
+    name: 'Happy Face',
+    email: 'really.dont.care@happyface.com',
+    password: 'keeysmiling',
+  },
+  '2cc689cd': {
+    id: '2cc689cd',
+    name: 'Grumpy Cat',
+    email: 'goodmorning.nosuchthing@fish.ca',
+    password: 'whereismycoffee',
+  },
+};
+
+
 const generateRandomString = () => {
   const shortURl = Math.random().toString(36).substring(2,8);
   return shortURl;
 };    
 
+app.post("/login", (req,res) => {    //????added to header partial, any changes to make to page depandences?
+  const username = req.body.username;
+  res.cookie('username', username);     ///????cookies application grey out???
+  res.redirect(`/urls`);                 //TO-DO:check cookie Name and the Value, make sure remain set, and no change for new user submition
+});
+
+//Use cURL to inspect the response from the new route
+//Terminal Testing with: curl -X POST -i localhost:8080/login -d "username=vanillaice"
+//The -d flag is used to send form data in the same way a browser would when submitting our login form.
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -81,13 +107,24 @@ app.get("/u/:shortURL", (req, res) => {   //Shorten URL Result & Click & Redirec
 });  
 
 
-app.post("/u/:shortURL", (req, res) => {  //URL Edit on ShortURL page & Update Database
-  const newLongURL = req.body.newLongURL;
-  // /urlDatabase[shortURL];
-  const shortURL = generateRandomString();
-  urlDatabase[shortURL] = newLongURL;
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index",templateVars);
+app.post("/urls/:editURL", (req, res) => {  //URL Edit on ShortURL page & Update Database
+  const editURL = req.body.editURL;
+  // // /urlDatabase[shortURL];
+  // urlDatabase[editURL] = newLongURL;
+  // // console.log("shortURL", shortURL);
+  const templateVars = { urls: urlDatabase, longURL: urlDatabase[editURL], shortURL: editURL};
+  res.render("urls_show",templateVars);
+});
+
+app.post("/urls/:id/editURL", (req,res) => {
+const newLongURL = req.body.newLongURL;
+const id = req.body.id;
+console.log(urlDatabase[id]);
+delete urlDatabase[id];
+urlDatabase[id] = newLongURL;
+console.log(urlDatabase[id]);
+const templateVars = { urls: urlDatabase }
+res.render("urls_index", templateVars);
 });
 
 
