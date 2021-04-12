7ce7fd0c63c6e922a01af4ab94971023d3dd2117
Liuming <lfan.canada@gmail.com>
2021-04-12 18:51:12 +0000

    added HTML content to res.send & use curl -i URL(cURL) to fetch URL

--- express_server.js
+++ express_server.js
@@ -12,7 +12,21 @@ app.get("/",(req,res) => {
 });
 
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);           //response with urlDatabase with JSON string;
+});
+
+
+app.get("/hello",(req,res) => {
+res.send("<html><body>Hello <b>World</b></body></html>\n");
+
+});
+
 app.listen(PORT,() => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+
+
+//curl -i http://localhost:8080/hello   
+//the HTML content that the /hello path responds with: <html><body>Hello <b>World</b></body></html>
