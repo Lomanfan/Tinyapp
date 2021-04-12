00ad8e306835d0635fa28da37f28fbf1d0cd6d47
Liuming <lfan.canada@gmail.com>
2021-04-12 20:12:07 +0000

    set view engine to ejs

--- express_server.js
+++ express_server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const app = express();
-const PORT = 8080; 
+const PORT = 8080;
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/",(req,res) => {
+app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
@@ -17,12 +19,24 @@ app.get("/urls.json", (req, res) => {
 });
 
 
-app.get("/hello",(req,res) => {
-res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 
 });
 
-app.listen(PORT,() => {
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+});
+
+
+
+app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
